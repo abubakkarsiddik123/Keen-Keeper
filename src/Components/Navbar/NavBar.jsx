79965a4d0d11d638fc97e1596313566e49ec17fb
abubakkarsiddik123 <abu089604@gmail.com>
2026-05-08 05:55:26 +0600

    make navbar responsive for mobile  devices

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -1,18 +1,22 @@
+
 import Image from "next/image";
 import MyNaveLink from "./MyNaveLink";
 
 const NavBar = () => {
   return (
-    <div className="shadow-sm  bg-base-100">
-      <div className="navbar max-w-[1600px] mx-auto  ">
-        <div className="navbar-start ">
+    <div className="shadow-sm bg-base-100">
+      <div className="navbar max-w-[1600px] mx-auto px-3 sm:px-4 md:px-6">
+        
+        <div className="navbar-start">
           <Image
             src="/logo.png"
             alt="keen-keeper"
             width={141}
             height={31}
-          ></Image>
+            className="w-[110px] sm:w-[125px] md:w-[141px] h-auto"
+          />
         </div>
+
         <div className="navbar-end">
           <MyNaveLink />
         </div>
