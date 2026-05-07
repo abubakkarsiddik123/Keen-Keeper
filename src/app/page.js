3cb2fa164d7be828b4bc9bda006c261372f551bd
abubakkarsiddik123 <abu089604@gmail.com>
2026-05-08 05:22:17 +0600

    fixed localhost fetch issue for production build

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Banner from "@/Components/HomePage/Banner";
 import Stats from "@/Components/HomePage/Stats";
 
 export default async function Home() {
-  const res = await fetch("http://localhost:3000/data.json");
+  const res = await fetch("https://keen-keeper-olive-zeta.vercel.app/data.json");
   const friends = await res.json();
   return (
     <div className="max-w-[1110px] mx-auto mt-16 mb-16 ">
