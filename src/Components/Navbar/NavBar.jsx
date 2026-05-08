d2a3c208e02e95345e592682ad72077a3810b23e
abubakkarsiddik123 <abu089604@gmail.com>
2026-05-08 14:53:00 +0600

    make navbar fully responsive for small devices

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -1,11 +1,10 @@
-
 import Image from "next/image";
 import MyNaveLink from "./MyNaveLink";
 
 const NavBar = () => {
   return (
     <div className="shadow-sm bg-base-100">
-      <div className="navbar max-w-[1600px] mx-auto px-3 sm:px-4 md:px-6">
+      <div className="navbar max-w-[1600px] mx-auto px-2 sm:px-4 md:px-6">
         
         <div className="navbar-start">
           <Image
@@ -13,12 +12,14 @@ const NavBar = () => {
             alt="keen-keeper"
             width={141}
             height={31}
-            className="w-[110px] sm:w-[125px] md:w-[141px] h-auto"
+            className="w-[80px] sm:w-[110px] md:w-[125px] lg:w-[141px] h-auto"
           />
         </div>
 
         <div className="navbar-end">
-          <MyNaveLink />
+          <div className="scale-75 sm:scale-90 md:scale-100 origin-right">
+            <MyNaveLink />
+          </div>
         </div>
       </div>
     </div>
